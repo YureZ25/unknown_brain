6f11ef8f24c90713d5378495112601b3eacc6072
Sagiriya <ermolov.pasha@mail.ru>
2022-11-26 01:06:00 +0300

    fix depinjection

--- BE/UB.Data/DataLayerDependecyInjection.cs
+++ BE/UB.Data/DataLayerDependecyInjection.cs
@@ -17,7 +17,7 @@ namespace UB.Data
             services.AddTransient<IUoW, UoW>();
 
             services.AddTransient<IUserRepository, UserRepository>();
-            services.AddTransient<GradeRepository>();
+            services.AddTransient<IGradeRepository,GradeRepository>();
         }
     }
 }
