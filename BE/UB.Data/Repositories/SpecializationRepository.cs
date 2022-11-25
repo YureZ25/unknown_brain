5976eb785f56c9dd0e5c115846ed9c0915cf0cd6
Nikk0Nikk0Ni <nickokar@mail.ru>
2022-11-26 00:02:45 +0300

    im added a interface specialization

---
+++ BE/UB.Data/Repositories/SpecializationRepository.cs
@@ -0,0 +1,14 @@
+﻿using UB.Data.Domain;
+using UB.Data.Repositories.Interfaces;
+
+namespace UB.Data.Repositories
+{
+    internal class SpecializationRepository : BaseRepository<User>, ISpecializationRepository
+    {
+        public SpecializationRepository(AppDbContext context) : base(context)
+        {
+        }
+
+
+    }
+}
