ffc14ef73269c1b2dbb913aa24138f3a2ae8af43
Sagiriya <ermolov.pasha@mail.ru>
2022-11-26 00:59:11 +0300

    add grade services&interface, Dto

---
+++ BE/UB.Services/Services/GradeService.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using UB.Data.Repositories.Interfaces;
+using UB.Services.Dtos;
+using UB.Services.Interfaces;
+
+namespace UB.Services.Services
+{
+    internal class GradeService : IGradeService
+    {
+        private readonly IGradeRepository _gradeService;
+        private readonly IMapper _mapper;
+
+        public GradeService(IGradeRepository gradeService, IMapper mapper)
+        {
+            _gradeService = gradeService;
+            _mapper = mapper;
+        }
+
+        public async Task<RespModel<IList<GradeDto>>> GetAllAsync()
+        {
+            var grades = await _gradeService.GetAllAsync();
+            var gradesDto = _mapper.Map<IList<GradeDto>>(grades);
+            return new RespModel<IList<GradeDto>>(gradesDto);
+        }
+    }
+}
