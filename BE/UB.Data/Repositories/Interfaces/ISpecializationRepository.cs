d88651695fb3df85660ea8534c915849f7f0db91
Nikk0Nikk0Ni <nickokar@mail.ru>
2022-11-26 01:26:17 +0300

    im added specialization and dto

--- BE/UB.Data/Repositories/Interfaces/ISpecializationRepository.cs
+++ BE/UB.Data/Repositories/Interfaces/ISpecializationRepository.cs
@@ -1,8 +1,9 @@
 ﻿using UB.Data.Domain;
 
+
 namespace UB.Data.Repositories.Interfaces
 {
-    public interface ISpecialization : IRepository<User>
+    public interface ISpecializationRepository : IRepository<User>
     {
 
     }
--- BE/UB.Data/Repositories/SpecializationRepository.cs
+++ BE/UB.Data/Repositories/SpecializationRepository.cs
@@ -1,5 +1,7 @@
 ﻿using UB.Data.Domain;
 using UB.Data.Repositories.Interfaces;
+using UB.Data.Domain;
+
 
 namespace UB.Data.Repositories
 {
