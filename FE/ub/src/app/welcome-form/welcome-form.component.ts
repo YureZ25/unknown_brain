449bac34c9383ac683e2a8ef2b14508cb9b84b39
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2022-11-26 07:05:11 +0300

    Add bootstrap to angular

---
+++ FE/ub/src/app/welcome-form/welcome-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-form',
+  templateUrl: './welcome-form.component.html',
+  styleUrls: ['./welcome-form.component.css']
+})
+export class WelcomeFormComponent {
+
+}
