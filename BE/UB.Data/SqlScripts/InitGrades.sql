43edb30658585b08025a3d65ae238f4dac4618b3
Yura <48731778+YureZ25@users.noreply.github.com>
2022-11-26 21:06:31 +0300

    Add recomendation entity (#12)

--- BE/UB.Data/SqlScripts/InitGrades.sql
+++ BE/UB.Data/SqlScripts/InitGrades.sql
@@ -1,9 +1,9 @@
 DELETE FROM public."Grades";
 
 INSERT INTO public."Grades"(
-	"Id", "Name")
+	"Id", "Name", "Order")
 VALUES
-    (gen_random_uuid(), 'Junior'),
-    (gen_random_uuid(), 'Middle'),
-    (gen_random_uuid(), 'Middle+'),
-    (gen_random_uuid(), 'Senior');
+    (gen_random_uuid(), 'Junior', 1),
+    (gen_random_uuid(), 'Middle', 2),
+    (gen_random_uuid(), 'Middle+', 3),
+    (gen_random_uuid(), 'Senior', 4);
