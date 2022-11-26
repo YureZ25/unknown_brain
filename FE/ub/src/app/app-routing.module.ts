adaab93776211135771b665c8e1cb600b85da44a
Yura <48731778+YureZ25@users.noreply.github.com>
2022-11-26 07:06:28 +0300

    Add bootstrap to angular (#5)
    Co-authored-by: Yuriy Nikitin <yuriy.nikitin@splzone.com>

--- FE/ub/src/app/app-routing.module.ts
+++ FE/ub/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeFormComponent } from './welcome-form/welcome-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FE/ub/src/app/app.module.ts
+++ FE/ub/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeFormComponent } from './welcome-form/welcome-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeFormComponent
   ],
   imports: [
     BrowserModule,
