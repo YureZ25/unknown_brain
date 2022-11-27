49d749ddbcb833effd7cfe03bae76380e0d7c124
Sagiriya <ermolov.pasha@mail.ru>
2022-11-27 10:33:11 +0300

    add sql script for recommendation

--- BE/UB.Data/SqlScripts/InitSpecializations.sql
+++ BE/UB.Data/SqlScripts/InitSpecializations.sql
@@ -3,10 +3,10 @@ DELETE FROM public."Specializations";
 INSERT INTO public."Specializations"(
 	"Id", "Name")
 VALUES 
-	(gen_random_uuid(), 'Frontend developer'),
-	(gen_random_uuid(), 'Backend developer'),
-	(gen_random_uuid(), 'Data Scientist'),
-	(gen_random_uuid(), 'QA Engineer'),
-	(gen_random_uuid(), 'UX/UI designer'),
-	(gen_random_uuid(), 'Product manager'),
-	(gen_random_uuid(), 'Project manager')
+	('768b2c70-bb4b-4a53-ab4b-859ec5a4ac66', 'Frontend developer'),
+	('e52fba2a-8769-4a23-9f1e-38cd7ef1dda5', 'Backend developer'),
+	('62927c5d-be45-4025-80f8-ccb489d368d3', 'Data Scientist'),
+	('be13ed96-e25b-4772-b7db-a6b3fece1886', 'QA Engineer'),
+	('ca21fab2-9acd-4f05-815c-43bcd6d6aa83', 'UX/UI designer'),
+	('71effd2d-6e19-4e82-9b62-7577ec8af0fb', 'Product manager'),
+	('39899140-9dbb-4ca2-befb-e8e39ec084a1', 'Project manager')
